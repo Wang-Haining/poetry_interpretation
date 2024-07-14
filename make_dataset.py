197eda0bf8fbb995b175e30daa13d8400fa1cb92
hw@chill <hw56@indiana.edu>
2024-07-14 14:26:26 -0400

    fix: rm index

--- make_dataset.py
+++ make_dataset.py
@@ -8,10 +8,10 @@ df = pd.read_csv(CSV_PATH)
 df_filtered = df[~df['Summary'].str.contains("No 'Summary' h2 tag found")]
 
 # split the dataset into training (80%), validation (10%), and test (10%) sets
-train_df = df_filtered.sample(frac=0.8, random_state=42)
-temp_df = df_filtered.drop(train_df.index)
-val_df = temp_df.sample(frac=0.5, random_state=42)
-test_df = temp_df.drop(val_df.index)
+train_df = df_filtered.sample(frac=0.8, random_state=42).reset_index(drop=True)
+temp_df = df_filtered.drop(train_df.index).reset_index(drop=True)
+val_df = temp_df.sample(frac=0.5, random_state=42).reset_index(drop=True)
+test_df = temp_df.drop(val_df.index).reset_index(drop=True)
 
 # convert DataFrames to Hugging Face datasets
 train_dataset = Dataset.from_pandas(train_df)
